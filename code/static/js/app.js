95b06d906876416d69b1c246126a3b7a872371aa
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-06-17 16:43:00 -0600

    building user select options

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -2,11 +2,9 @@
 // d3.json to grab the metadata for a sample
 // d3 to select the id '#sample-metadata' in the index.html and .html("") to clear it initially
 // Create a url for hosting the metdata sample 
-function buildMetadata(sample) {
-    var sample_meta_panel = d3.select("#sample-metadata").html("");
-    var metadata_url = `/metadata/${sample}`;
-
-
-
-
+function init() {
+    d3.json("data/samples.json")
+    data = starterData;
+    var values = starterData.names;
+    var userSelect = d3.select('#selDataset');
 }
