d76c5b1ba47a5146ca7fe5e78ce62530bdf12c2a
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-06-17 16:59:54 -0600

    For Each loop to return values for the user selection

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -1,10 +1,19 @@
-// Build the function for the metadata panel
+// Initiate the function for the metadata panel
 // d3.json to grab the metadata for a sample
-// d3 to select the id '#sample-metadata' in the index.html and .html("") to clear it initially
-// Create a url for hosting the metdata sample 
+// d3 to select the id '#selDataset' in the index.html
+var data;
 function init() {
     d3.json("data/samples.json")
     data = starterData;
     var values = starterData.names;
     var userSelect = d3.select('#selDataset');
+    valueReturn.forEach(value => {
+        userSelect
+            .append("option")
+            .text(value)
+            .attr("value", function(){
+                return value;
+            });
+    
+    });
 }
