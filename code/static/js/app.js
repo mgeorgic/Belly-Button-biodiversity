31a1832752595a1ae83ea0affe7d0897439d97bb
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-06-17 17:31:02 -0600

    Indentation problems and end syntax resolved

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -3,17 +3,22 @@
 // d3 to select the id '#selDataset' in the index.html
 var data;
 function init() {
-    d3.json("data/samples.json")
-    data = starterData;
-    var values = starterData.names;
-    var userSelect = d3.select('#selDataset');
-    valueReturn.forEach(value => {
-        userSelect
-            .append("option")
-            .text(value)
-            .attr("value", function(){
-                return value;
+    d3.json("data/samples.json").then(dataInitial =>{
+        data = starterData;
+        var uservalues = starterData.names;
+        var userSelect = d3.select('#selDataset');
+        valueReturn.forEach(value => {
+            userSelect
+                .append("option")
+                .text(value)
+                .attr("value", function(){
+                    return value;
+                });
             });
-    
-    });
+        });
 }
+// initate the function to fill values for user's selection
+init()
+
+// Use d3 to grab the dataset selected and change it upon user's selection
+d3.selectAll("#selDataset").on("change",plotFunctions)
