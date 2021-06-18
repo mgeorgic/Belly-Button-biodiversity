15193a133ceabf3fd66954cf8c6bb4fb3259708d
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-06-18 08:51:34 -0600

    added commenture to ensure people reading code understands logic

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -23,6 +23,7 @@ init()
 // Use d3 to grab the dataset selected and change it upon user's selection
 d3.selectAll("#selDataset").on("change",plotFunctions);
 
+// Create the plot function and where to grab the values for each plot
 function plotFunctions() {
     var valueReturn = d3.select("#selDataset").node().value;
     demoFunct(valueReturn);
@@ -32,10 +33,12 @@ function plotFunctions() {
     gaugeGraph(valueReturn);
 }
 
+// Create demographic map where only top ten are returned
+// for id, otu_id, sample_values, otu_labels
 function demographicFunct(valueReturn) {
     var filterID = data.samples.filter(value => value.id == userSelect);
     var otuID = filterID.map(val => val.out_ids);
-    otuID =treatotuID(otuID[0].slice(0,10));
+    otuID = treatotuID(otuID[0].slice(0,10));
     var sampleVal = filterID.map(val =>val.sample_values);
     sampleVal= sampleVal[0].slice(1,10);
 
@@ -45,7 +48,8 @@ function demographicFunct(valueReturn) {
     console.log(sampleVal);
     console.log(output_name)
     console.log(names);
-    
+
+
 
 
 
