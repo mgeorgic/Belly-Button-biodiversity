f519e8486b30ba817c49bc7ba84f92f2a3a65463
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-06-18 18:12:03 -0600

    Bubble chart and demographic panel complete

--- code/js/app.js
+++ code/js/app.js
@@ -36,20 +36,20 @@ function renderProcess(){
         // Puts the first sample into the result variable 
         var result = resultArray[0];
 
-        // gathers variables that hold the chart value parts
+        // Variables for the chart values 
         var otu_ids = result.otu_ids;
         var samp_val = result.sample_values;
         var otu_lab = result.otu_labels;
 
-        // reformats the x, y ticks and labels for the bar chart in decending order and adds the OTU to the y ticks
+        // Reformats the x, y ticks and labels in decending order and adds the OTU to the y ticks
 
-        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU_ID ${otuID}`).reverse();
         var xticks = samp_val.slice(0, 10).reverse();
         var labels = otu_lab.slice(0, 10).reverse();
 
-        console.log(yticks);
-        console.log(xticks);
-        console.log(labels);
+        //console.log(yticks);
+        //console.log(xticks);
+        //console.log(labels);
 
         var trace1 = {
             x: xticks,
@@ -62,10 +62,75 @@ function renderProcess(){
         var data = [trace1] 
 
         var layout = {
-            title: 'top 10 OTUs Found'
+            title: 'Top Ten OTUs Present in all Belly Button Samples'
         };
         
         Plotly.newPlot('bar', data, layout);
 
+  // Bubble Chart 
 
-      
+        // Variables to fetch the chart's data
+        var bubbleX = otu_ids.slice(0, 10);
+        bubbleX.push.apply(bubbleX, otu_ids.slice(Math.max(otu_ids.length)));
+
+        var bubbleY = samp_val.slice(0, 10);
+        bubbleY.push.apply(bubbleY, samp_val.slice(Math.max(samp_val.length)));
+
+        var bubblelab = otu_lab.slice(0, 10);
+        bubblelab.push.apply(bubblelab, otu_lab.slice(Math.max(otu_lab.length)));
+
+        //console.log(xticksbub);
+        //console.log(labelsbub);
+        //console.log(yticksbub);
+
+        // plots data
+        var trace1 = {
+            x: bubbleX,
+            y: bubbleY,
+            mode: 'markers',
+            marker: {
+              color: bubbleX,  
+              size: bubbleY,
+              colorscale: "Earth"
+            },
+            text: bubblelab
+          };
+          
+          var data = [trace1];
+          
+          var layout = {
+            title: 'Entire OTU Sample Population',
+            xaxis: {
+                title: "OTU-ID"
+            },
+            showlegend: false,
+            height: 700,
+            width: 1300
+          };
+          
+          Plotly.newPlot('bubble', data, layout);
+
+        // Demographic Chart
+
+        // Fetches the metadata by user's input
+        var resultArray2 = metadata.filter(metaObj => metaObj.id == TestID);
+
+        console.log(resultArray2);
+
+        var result2 = resultArray2[0];
+
+        // Parses out all demos into variables
+
+        var age = result2.age;
+        var bbtype = result2.bbtype;
+        var ethnicity = result2.ethnicity;
+        var gender = result2.gender;
+        var id = result2.id;
+        var location = result2.location;
+        var wfreq = result2.wfreq;
+
+        // Key and value formatting
+        // place the key and value in the HTML at the correct id
+        var demoinfo = ` id: ${id} <br> ethnicity: ${ethnicity} <br> gender: ${gender} <br> age: ${age} <br> location: ${location} <br> bbtype: ${bbtype} <br> wfreq: ${wfreq}`
+        document.getElementById('sample-metadata').innerHTML = demoinfo;
+
